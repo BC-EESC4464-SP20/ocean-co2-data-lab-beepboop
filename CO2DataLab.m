1b9701d2249cb40866948c9922414199d3bfbf72
sarahashebir <ashebir@bc.edu>
2020-03-22 23:04:46 -0500

    we meet again
    we meet again, github, we meet again ~audible sigh~

--- CO2DataLab.m
+++ CO2DataLab.m
@@ -70,12 +70,12 @@ geoshow('landareas.shp','FaceColor','black')
 title('January Sea Surface Temperature (^oC)')
 
 
-
+%%
 %Check that you can make a similar type of global map for another month
 %and/or for pCO2 using this approach. Check the documentation and see
 %whether you can modify features of this map such as the contouring
 %interval, color of the contour lines, labels, etc.
-%%
+
 figure(2)
 worldmap world
 contourfm(latgrid, longrid, SSTgrid(:,:,5),'linecolor','none');
@@ -135,7 +135,7 @@ PCO2atTmean = PCO2_SWgrid .* exp(0.0423*(TMean_rep - SSTgridMean));
 PCO2atTobs = PCO2SWAnnMean .* exp(0.0423*(SSTgridMean - TMean_rep));
 %temperature,, 
 
-%% i dont think we need any of this
+%% i dont think we need any of this // jk we do
 %Equation 3
 Tmax_bio = nanmax(PCO2atTmean,[],3);
 Tmin_bio = nanmin(PCO2atTmean,[],3);
@@ -163,38 +163,95 @@ BATS_PCO2SW = squeeze(PCO2_SWgrid(BATS_latloc,BATS_lonloc,:));
 BATS_PCO2bio = squeeze(PCO2atTmean(BATS_latloc,BATS_lonloc,:));
 BATS_PCO2t = squeeze(PCO2atTobs(BATS_latloc,BATS_lonloc,:));
 
-hold on
+figure(5); clf
+subplot(2,2,1) %creates a 2 x 1 grid of subplots, and then starts plotting in the 1st of the subplots
 plot(BATS_SST)
+subplot(2,2,2) %sticks with the same 2 x 1 grid, but now plotting in the 2nd subplot (3rd input)
 plot(BATS_PCO2SW)
+subplot(2,2,3)
 plot(BATS_PCO2bio)
+subplot(2,2,4)
 plot(BATS_PCO2t)
-hold off
 
-%% Perhaps Wrong
-% % SSTgrid(BATS_lat,BATS_lon,:);
-% BATS = NaN(4,12);
-%     BATS_SST = SSTgrid(28,60,:);
-%     BATS_PCO2SW = PCO2_SWgrid(28,60,:);
-%     BATS_PCO2bio = PCO2atTmean(28,60,:);
-%     BATS_PCO2t = PCO2atTobs(28,60,:);
-%   BATS =[BATS_SST, BATS_PCO2SW, BATS_PCO2bio,BATS_PCO2t]
-%   
-%   
-% %   PCO2_SWgrid(x,y,z)=CO2data.PCO2_SW(i);
-% %     subplot(BATS_SST,BATS_PCO2SW,BATS_PCO2bio);
-% %Station P (lat = 50N 145W)-->(33,44,:)
-%     StatP_SST = SSTgrid(33,44,:);
-%     StatP_PCO2SW = PCO2_SWgrid(33,44,:);
-%     StatP_PCO2bio = PCO2atTmean(33,44,:);
-%     StatP_PCO2t = PCO2atTobs(33,44,:);
-% 
-% %Ross Sea (76.5S ,, 176E) --> (1, 36, :)
-%     Ross_SST = SSTgrid(1, 36, :);
-%     Ross_PCO2SW = PCO2_SWgrid(1, 36, :);
-%     Ross_PCO2bio = PCO2atTmean(1, 36, :);
-%     Ross_PCO2t = PCO2atTobs(1, 36, :);
+%Station P (lat = 50N 145W)-->(33,44,:)
+[StatP_lat,StatP_latloc] = min(abs(latgrid-50));
+[StatP_lon, StatP_lonloc] = min(abs(longrid-(-145+360)));
+StatP_SST = squeeze(SSTgrid(StatP_latloc,StatP_lonloc,:));
+StatP_PCO2SW = squeeze(PCO2_SWgrid(StatP_latloc,StatP_lonloc,:));
+StatP_PCO2bio = squeeze(PCO2atTmean(StatP_latloc,StatP_lonloc,:));
+StatP_PCO2t = squeeze(PCO2atTobs(StatP_latloc,StatP_lonloc,:));
+
+figure(6); clf
+subplot(2,2,1) %creates a 2 x 1 grid of subplots, and then starts plotting in the 1st of the subplots
+plot(StatP_SST)
+subplot(2,2,2) %sticks with the same 2 x 1 grid, but now plotting in the 2nd subplot (3rd input)
+plot(StatP_PCO2SW)
+subplot(2,2,3)
+plot(StatP_PCO2bio)
+subplot(2,2,4)
+plot(StatP_PCO2t)
+
+%Ross Sea (76.5S ,, 176E) --> (1, 36, :)
+[Ross_lat,Ross_latloc] = min(abs(latgrid-(-76.5)));
+[Ross_lon, Ross_lonloc] = min(abs(longrid-176));
+Ross_SST = squeeze(SSTgrid(Ross_latloc,Ross_lonloc,:));
+Ross_PCO2SW = squeeze(PCO2_SWgrid(Ross_latloc,Ross_lonloc,:));
+Ross_PCO2bio = squeeze(PCO2atTmean(Ross_latloc,Ross_lonloc,:));
+Ross_PCO2t = squeeze(PCO2atTobs(Ross_latloc,Ross_lonloc,:));
+
+figure(7); clf
+subplot(2,2,1) %creates a 2 x 1 grid of subplots, and then starts plotting in the 1st of the subplots
+plot(Ross_SST)
+subplot(2,2,2) %sticks with the same 2 x 1 grid, but now plotting in the 2nd subplot (3rd input)
+plot(Ross_PCO2SW)
+subplot(2,2,3)
+plot(Ross_PCO2bio)
+subplot(2,2,4)
+plot(Ross_PCO2t)
+
 %% 8. Reproduce your own versions of the maps in figures 7-9 in Takahashi et al. 2002
 % But please use better colormaps!!!
 % Mark on thesese maps the locations of the three stations for which you plotted the
 % seasonal cycle above
+
+%Equation 3
+Tmax_bio = nanmax(PCO2atTmean,[],3);
+Tmin_bio = nanmin(PCO2atTmean,[],3);
+PCO2bio = Tmax_bio - Tmin_bio;
+
+figure(7)
+worldmap world
+contourfm(latgrid, longrid, PCO2bio(:,:,:),'linecolor','none');
+colormap(cmocean('balance', 'pivot', 0));
+colorbar
+geoshow('landareas.shp','FaceColor','black')
+plotm(40.702, 29.431, 'or', 'MarkerSize', 10, 'MarkerFaceColor', 'r'); %edit,, do 3 times
+title('May Sea Surface Temperature (^oC)')
+
+%Equation 4 ~CAUSING MAJOR ISSUES~
+Tmax_temp = nanmax(PCO2atTobs,[],3);
+Tmin_temp = nanmin(PCO2atTobs,[],3);
+PCO2temp = Tmax_temp - Tmin_temp;
+
+figure(8)
+worldmap world
+contourfm(latgrid, longrid, PCO2temp(:,:,:),'linecolor','none');
+colormap(cmocean('balance', 'pivot', 0));
+colorbar
+geoshow('landareas.shp','FaceColor','black')
+title('May Sea Surface Temperature (^oC)')
+
+%Equation 5
+Relative_PCO2 = PCO2temp - PCO2bio;
+% %negative values where bio is stronger // positive values where temp is
+% %stronger
+
+figure(9)
+worldmap world
+contourfm(latgrid, longrid, Relative_PCO2(:,:,:),'linecolor','none');
+colormap(cmocean('balance', 'pivot', 0));
+colorbar
+geoshow('landareas.shp','FaceColor','black')
+title('May Sea Surface Temperature (^oC)')
+
 
